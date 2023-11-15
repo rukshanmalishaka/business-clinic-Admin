7cb00c31f1b81778a102824c9159ef38da4d0133
Rukshan Malishaka <93638987+rukshanmalishaka@users.noreply.github.com>
2023-11-15 16:26:22 +0530

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,6 @@ $dbName     = "";
 // Create database connection
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-// Check connection
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
